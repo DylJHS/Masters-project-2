b3a0547ecf25a07c1605a2f62dd3df9f5ab3852d
Dylan <dylanhsimmons@hotmail.fr>
2024-04-09 13:47:58 +0200

    Integration of the mds script into the wider counts exploration script

--- Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/tcga_cancer_counts_mds.r
+++ Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/tcga_cancer_counts_mds.r
@@ -1,76 +1,53 @@
----
-title: "tcga_cancer_counts_xplr_mds"
-output: html_document
-date: "2024-04-08"
-Notes: This script is part of the counts exploraiton script and is intended to run the MDS for the data.
----
+# This script is part of the counts exploraiton script and is intended to run the MDS for the data.
 
-```{r setup, include=FALSE}
-knitr::opts_knit$set(root.dir = "/Users/Dyll/Documents/Education/VU_UVA/Internship/Epigenetics/Janssen_Group-UMCUtrecht/Main_Project")
-```
-
-
-```{r}
 # Load the packages
 library(plyr)
 library(dplyr)
 library(tidyverse)
 library(ggpubr)
-```
+library(conflicted)
 
-REUSABLE FUNCTIONS
-
-```{r}
 extract_element <- function(strings, index) {
   # Split each string by "." and extract the third element
   element_list <- sapply(strsplit(strings, "\\."), function(x) x[index])
   return(element_list)
 }
 
-```
+print("packages loaded")
 
-```{r}
 # Load the transposed data.
-# t_data <- read.csv("Data/RNA_Data/TCGA_Norm/Transposed_scaled_&_reduced_df.csv")
-test <- t_data[0:5000, 0:5000]
+t_data <- read.csv("Transposed_scaled_&_reduced_df.csv")
+# t_data <- t_data[0:5000,0:5000]
+print("t_data")
+print(head(t_data[, 1:10]))
 print(dim(t_data))
 
-soi <- read.csv("Data/RNA_Data/TCGA_TPM/TCGA_mRNA_TPM_SOI.csv")
+soi <- read.csv("../../../data/TCGA_mRNA_TPM_SOI.csv")
 soi_genes <-soi[,2]
-```
 
-
-```{r}
 # Filter the data down to the Set of interest.
-filtered_transposed <- test[, c(TRUE, colnames(test)[-1] %in% soi_genes)]
+intermediate_data <- t_data %>% column_to_rownames(var = "X")
+filtered_transposed <- intermediate_data[, colnames(intermediate_data) %in% soi_genes]
+print("filtered data")
 print(dim(filtered_transposed))
-```
 
-
-```{r}
 # Create the distance matrix on the filtered data.
 dist_matrix <- filtered_transposed %>%
   dist() 
 
-```
+write.csv(dist_matrix, "sample_dist_matrix.csv", row.names = TRUE)
 
-
-```{r}
 # Produce the MDS object.
 mds <- cmdscale(dist_matrix)
-```
 
-
-```{r}
 # Convert the MDS object to a dataframe with additional features
 mds_df <- as.data.frame(mds) %>% 
-  mutate(cancer_type = extract_element(filtered_transposed$X, 1),
-         sample_type = extract_element(filtered_transposed$X, 3))
-
-```
+  dplyr::mutate(cancer_type = extract_element(rownames(filtered_transposed), 1),
+         sample_type = extract_element(rownames(filtered_transposed), 3))
 
 
-```{r}
+write.csv(mds_df, "sample_mds.csv", row.names = TRUE)
+
 # Create the plots of the MDS.
 sample_mds <- ggscatter(mds_df, x = "V1", y = "V2", 
           color = "sample_type", # Colour based on the sample type
@@ -82,10 +59,7 @@ cancer_mds <- ggscatter(mds_df, x = "V1", y = "V2",
           size = 1,
           repel = TRUE)+
   theme(legend.position = "none")
-```
 
-
-```{r}
 # Save the plots.
 ggsave("sample_mds_plot.png",   
        plot = sample_mds,       
@@ -98,5 +72,3 @@ ggsave("cancer_mds_plot.png",
        width = 8,              
        height = 6,             
        dpi = 300) 
-```
-
--- Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/tcga_cancer_counts_xplr_v4.r
+++ Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/tcga_cancer_counts_xplr_v4.r
@@ -1,100 +1,81 @@
----
-title: "TCGA_RNA_COUNTS_XPLR"
-format: html
-editor: visual
-Notes: This is a script that is intended to do some exploration of the samples in teh count based TCGA PanCan dataset.
----
+# Load the libraries
+print("Started")
 
-```{r setup, include=FALSE}
-knitr::opts_knit$set(root.dir = "/Users/Dyll/Documents/Education/VU_UVA/Internship/Epigenetics/Janssen_Group-UMCUtrecht/Main_Project")
-```
-
-## Load the libraries
-
-```{r}
+library(conflicted)
 library(dplyr)
-library(plyr)
 library(knitr)
 library(tidyverse)
 library(edgeR)
 library(limma)
 library(data.table)
-library(ggpubr)
-```
 
-Load other options
+print("Loaded packages \n")
 
-```{r}
-getOption("max.print")
-```
+# REUSABLE FUNCTIONS
 
-REUSABLE FUNCTIONS
-
-```{r}
 extract_element <- function(strings, index) {
   # Split each string by "." and extract the third element
-  element_list <- sapply(strsplit(strings, "\\."), function(x) x[index])
+  element_list <- sapply(stringr::str_split(strings, "\\."),
+                         function(x) x[index])
   return(element_list)
 }
 
-untransform <- function(x) {#Function to convert the log transformed counts back into original counts
-  return(ceiling((2^x)-1))
+untransform <- function(x) {
+  # Function to convert the log transformed counts back into original counts
+  return(ceiling((2^x) - 1))
 }
-```
 
-Load the datasets
-
-```{r}
+# Load the datasets
 
 # RNAseq count data
-# original <- read.csv("Data/RNA_Data/TCGA_Norm/tcga_gene_expected_count.csv")
-test <- original[0:10000,0:10]
+original <- read.csv("tcga_gene_expected_count.csv")
+# test <- original[0:10000,0:15]
 
 # Metadata
-tss_meta <- read.csv("Data/Other/TCGA_meta/tissueSourceSite.tsv", sep = "\t")
-abbrv_meta <- read.csv("Data/Other/TCGA_meta/bcrBatchCode.tsv", sep = "\t")
-sampletype <- read.csv("Data/Other/TCGA_meta/sampleType.tsv", sep = "\t", colClasses = c("character"))
+tss_meta <- read.csv("../../../data/meta/tissueSourceSite.tsv", sep = "\t")
+abbrv_meta <- read.csv("../../../data/meta/bcrBatchCode.tsv", sep = "\t")
+sampletype <- read.csv("../../../data/meta/sampleType.tsv",
+  sep = "\t", colClasses = c("character")
+)
 
 # Gene Metadata
-gene_ids <- read.delim("Data/Other/TCGA_meta/TCGA_PanCan_TPM_Gene_Annotations.txt")
+gene_ids <- read.delim(
+  "../../../data/meta/TCGA_PanCan_TPM_Gene_Annotations.txt"
+)
 
 # SOI genes
-# soi <- read.csv("Data/RNA_Data/TCGA_TPM/TCGA_mRNA_TPM_SOI.csv")
+soi <- read.csv("../../../data/TCGA_mRNA_TPM_SOI.csv")
 soi_genes <-soi[,2]
-```
 
-```{r}
-transformed_data <- test
+print("Loaded datasets \n")
+
+
+transformed_data <- original
 rownames(transformed_data) <- NULL
 
 real_count_data <- transformed_data %>%
   mutate_at(vars(-1), untransform)
-```
 
-Format the data
+# Format the data
 
-```{r}
-count_data_ordered <- real_count_data[,order(colnames(real_count_data))]
+count_data_ordered <- real_count_data[, order(colnames(real_count_data))]
 
 # Combine the metadata
-meta <- left_join(tss_meta %>% 
-                     dplyr::select(c("TSS.Code", "Study.Name")) %>% 
-                     distinct() %>% 
-                     sapply(trimws) %>% 
-                     as.data.frame(),
-                   abbrv_meta %>%
-                     dplyr::select(c("Study.Abbreviation", "Study.Name")) %>% 
-                     distinct()%>% 
-                     sapply(trimws) %>% 
-                     as.data.frame(), 
-                   by = "Study.Name")
+meta <- dplyr::left_join(tss_meta %>%
+                           dplyr::select(c("TSS.Code", "Study.Name")) %>%
+                           distinct() %>%
+                           sapply(trimws) %>%
+                           as.data.frame(),
+                         abbrv_meta %>%
+                           dplyr::select(c("Study.Abbreviation", "Study.Name")) %>% 
+                           distinct()%>% 
+                           sapply(trimws) %>% 
+                           as.data.frame(), 
+                         by = "Stud                 y.Name")
 
-```
+# CONVERT THE GENE IDS INTO GENE NAMES
 
-CONVERT THE GENE IDS INTO GENE NAMES
-
-```{r}
-counts_data <- right_join(gene_ids %>% 
+counts_data <- dplyr::right_join(gene_ids %>% 
                     dplyr::select(c("id", "gene")) %>% 
                     sapply(trimws) %>% 
                     as.data.frame(),
@@ -104,66 +85,61 @@ counts_data <- right_join(gene_ids %>%
 
 count_data <- counts_data %>%
   mutate(gene = trimws(gene))
-  
-```
 
-2.  Edit the samples (column names) into ids so as to be able to identify the participant and the sample and cancer type
+print("\n count_data")
+print(head(count_data[, 1:10]))
 
-2.1. extract the different elements from the sample names
 
-```{r}
+# 2.  Edit the samples (column names) into ids so as to be able to identify the participant and the sample and cancer type
+
+# 2.1. extract the different elements from the sample names
+
 ids <- colnames(count_data)[-1]
 participants <- extract_element(ids, 3)
 condition <- extract_element(ids, 4)
 tissue_type <- extract_element(ids, 2)
-```
 
-2.2. Reform the column names using the extracted elements and reset the index as the Genes
+# 2.2. Reform the column names using the extracted elements and reset the index as the Genes
 
-```{r}
 column_names <- c("Genes" ,  paste(tissue_type, participants, condition, sep = "."))
 colnames(count_data) <- column_names
-```
 
-Convert data to a data.table for faster processing during the grouping of the duplicate genes
-
-```{r}
-count_data <- setDT(count_data)  
+# Convert data to a data.table for faster processing during the grouping of the duplicate genes
+count_data <- data.table::setDT(count_data)
 
 # Combine duplicate genes together using the median of the expression
 grouped <- count_data[, lapply(.SD, function(x) if (length(x) > 1) ceiling(median(x, na.rm = TRUE)) else x), by = Genes, .SDcols = -"Genes"]
-```
 
-```{r}
 data <- grouped %>%
   column_to_rownames(var = "Genes")
 
+print("\n grouped")
+print(head(grouped[, 1:10]))
+
+
 # Remove the unwanted sample types from the samples based on the code
-codes_to_use <- c("01","02","03","04","05","08","09")
+codes_to_use <- c("01", "02", "03", "04", "05", "08", "09")
 samples_to_use <- data %>%  dplyr::select(ends_with(codes_to_use))
-```
 
-2.3. Assign the TSS abbreviation to the column name in place of the TSS code
+# 2.3. Assign the TSS abbreviation to the column name in place of the TSS code
 
-```{r}
 column_names <- colnames(samples_to_use)
 prefixes <- substr(column_names,1,2)
-abbrvs <- meta$Study.Abbreviation[match(prefixes,meta$TSS.Code)]
+abbrvs <- meta$Study.Abbreviation[match(prefixes, meta$TSS.Code)]
 column_names_prefixes <- paste0(abbrvs, substr(column_names, 3, nchar(column_names)))
 colnames(samples_to_use) <- column_names_prefixes
-```
 
-2.4. Assign the sample type abbreviation to the column name in place of the sample type code
+# 2.4. Assign the sample type abbreviation to the column name in place of the sample type code
 
-```{r}
 new_column_names <- colnames(samples_to_use)
 suffixes <- substr(new_column_names, nchar(new_column_names) - 1, nchar(new_column_names))
-s_abbrvs <- sampletype$Short.Letter.Code[match(suffixes,sampletype$Code)]
+s_abbrvs <- sampletype$Short.Letter.Code[match(suffixes, sampletype$Code)]
 column_names_suffxes <- paste0(substr(new_column_names, 1, nchar(new_column_names) - 2), s_abbrvs)
 colnames(samples_to_use) <- column_names_suffxes
-```
 
-```{r}
+print("\n samples_to_use")
+print(head(samples_to_use[, 1:10]))
+
 # Remove 0 genes and samples
 unzero_d <- samples_to_use[rowSums(samples_to_use[]) > 0, ] # samples
 cols_zeros <- which(apply(unzero_d, 2, function(x) all(x == 0)))
@@ -172,10 +148,7 @@ if (length(cols_zeros) > 0) {
 } else {
   reduced <- unzero_d
 }
-```
 
-
-```{r}
 # convert df to matrix 
 mapped_matrix <- as.matrix(reduced)
 
@@ -186,33 +159,31 @@ d0 <- edgeR::calcNormFactors(mapped_matrix, method = "TMM")
 # Divide the samples by their corresponding normalisation factors
 scaled_data <- base::sweep(samples_to_use, 2, d0, "/") # scalling the genes (2 = columns)
 scaled_data <- ceiling(scaled_data) # Round up
-```
 
-
-```{r}
 # Filter out non SOI genes
 filtered_scalled <- scaled_data[rownames(scaled_data) %in% soi_genes, ]
+print("filtered data")
 print(dim(filtered_scalled))
 
 # Log Transform the counts
 log_data <- apply(filtered_scalled, MARGIN = c(1,2), FUN = function(x) log(x + 1))
-```
 
-
-```{r}
 # 5.  MDS
 # Transpose the data 
 t_scaled_log_data <- t(log_data)
-```
+write.csv(t_scaled_log_data, "Transposed_scaled_&_reduced_df.csv", row.names = TRUE)
 
+print("\n t_scaled_log_data")
+print(head(t_scaled_log_data[, 1:3]))
 
-```{r}
 sample_dist_matrix <- dist(t_scaled_log_data)
 sample_dist_df <- as.data.frame(as.matrix(sample_dist_matrix))
-```
 
+print("\n sample_dist_matrix")
+print(head(sample_dist_df[, 1:10]))
 
-```{r}
+write.csv(sample_dist_df, "sample_dist_matrix.csv", row.names = TRUE)
+
 # Produce the MDS object.
 mds <- cmdscale(sample_dist_matrix)
 
@@ -220,10 +191,10 @@ mds <- cmdscale(sample_dist_matrix)
 mds_df <- as.data.frame(mds) %>% 
   dplyr::mutate(cancer_type = extract_element(rownames(t_scaled_log_data), 1),
          sample_type = extract_element(rownames(t_scaled_log_data), 3))
-```
 
 
-```{r}
+write.csv(mds_df, "sample_mds.csv", row.names = TRUE)
+
 # Create the plots of the MDS.
 sample_mds <- ggscatter(mds_df, x = "V1", y = "V2", 
           color = "sample_type", # Colour based on the sample type
@@ -235,10 +206,7 @@ cancer_mds <- ggscatter(mds_df, x = "V1", y = "V2",
           size = 1,
           repel = TRUE)+
   theme(legend.position = "none")
-```
 
-
-```{r}
 # Save the plots.
 ggsave("sample_mds_plot.png",   
        plot = sample_mds,       
@@ -250,6 +218,4 @@ ggsave("cancer_mds_plot.png",
        plot = cancer_mds,       
        width = 8,              
        height = 6,             
-       dpi = 300)
-```
-
+       dpi = 300) 
