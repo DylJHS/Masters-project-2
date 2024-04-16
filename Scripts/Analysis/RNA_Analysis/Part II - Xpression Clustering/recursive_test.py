e18389eda7f4a8fb21c9dc329741f1e361dee4fb
Dylan <dylanhsimmons@hotmail.fr>
2024-04-16 10:40:48 +0200

    Changes and implementation of the Intra-CCS to the clstering script

--- Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/recursive_test.py
+++ Scripts/Analysis/RNA_Analysis/Part II - Xpression Clustering/recursive_test.py
@@ -4,21 +4,18 @@ import matplotlib.pyplot as plt
 np.random.seed(33)
 
 # Between -1 and 1 (normal)
-def recurs_test(loc, size):
-    norm_correlation_values = list(np.random.normal(loc = loc, scale = 0.1, size = size))
-    # norm_correlation_values  = [x - np.mean(numbers_list) for x in numbers_list]
-    # print(norm_correlation_values)
-    norm_naked_sum = 0
-    for r in norm_correlation_values:
-        w = 2 if r > 0 else 1
-        norm_naked_sum += w * (r**2)
-    result = norm_naked_sum / len(norm_correlation_values)
-    # output = f'Size: {size} : \n result = {norm_naked_sum} \n'
-    return result
+# def recurs_test(loc, size):
+#     norm_correlation_values = list(np.random.normal(loc = loc, scale = 0.1, size = size))
+#     norm_naked_sum = 0
+#     for r in norm_correlation_values:
+#         w = 2 if r > 0 else 1
+#         norm_naked_sum += w * (r**2)
+#     result = norm_naked_sum / len(norm_correlation_values)
+#     return result
 
 
-print(recurs_test(0.2, 1000))
-print(recurs_test(-0.2, 1000))
+# print(recurs_test(0.2, 1000))
+# print(recurs_test(-0.2, 1000 ))
 
 
 # fig1 = plt.figure()
@@ -36,3 +33,15 @@ print(recurs_test(-0.2, 1000))
 
 
 
+
+norm_correlation_values = list(np.random.normal(loc = 0, scale = 0.1, size = 10))
+# norm_naked_sum = 0
+# for r in norm_correlation_values:
+# w = 2 if r > 0 else 1
+# norm_naked_sum += w * (r**2)
+# result = norm_naked_sum / len(norm_correlation_values)
+# return result
+
+
+
+print(norm_correlation_values)
