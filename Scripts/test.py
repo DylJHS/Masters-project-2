f7a1f630c8a65ba718dac467fc78306f1f7250e0
Dylan <dylanhsimmons@hotmail.fr>
2024-04-24 11:02:34 +0200

    Updates to the scripts for the clustering

---
+++ Scripts/test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+df = pd.read_csv("/Users/Dyll/Documents/Education/VU_UVA/Internship/Epigenetics/Janssen_Group-UMCUtrecht/Main_Project/Scripts/Analysis/RNA_Analysis/DGEA_ALLGENES_BYTISSUE_XPRSS_TBL.csv")
+
+
+print(df.head())
