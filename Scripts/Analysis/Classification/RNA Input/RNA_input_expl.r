1b2cb70b16e7232662134be6f4b59eca3bf76952
Dylan <dylanhsimmons@hotmail.fr>
2024-06-04 21:46:02 +0200

    Changes to the RNA scripts for input into the model

---
+++ Scripts/Analysis/Classification/RNA Input/RNA_input_expl.r
@@ -0,0 +1,61 @@
+library(dplyr)
+library(ggplot2)
+
+
+path <- "/Users/Dyll/Documents/Education/VU_UVA/Internship/Epigenetics/Janssen_Group-UMCUtrecht/Main_Project/"
+setwd(path)
+
+tss_meta <- read.csv("Data/Other/TCGA_meta/tissueSourceSite.tsv", sep = "\t")
+abbrv_meta <- read.csv("Data/Other/TCGA_meta/bcrBatchCode.tsv", sep = "\t")
+meta <- left_join(
+  tss_meta %>%
+    dplyr::select(c("TSS.Code", "Study.Name")) %>%
+    distinct() %>%
+    sapply(trimws) %>%
+    as.data.frame(),
+  abbrv_meta %>%
+    dplyr::select(c("Study.Abbreviation", "Study.Name")) %>%
+    distinct() %>%
+    sapply(trimws) %>%
+    as.data.frame(),
+  by = "Study.Name"
+)
+
+example_set <- read.csv(
+  "Data/RNA_Data/log_scld_tpm_soi.csv"
+) %>%
+  select("X") %>%
+  mutate(other = vapply(
+                        strsplit(X, "\\."),
+                        `[`, 2, FUN.VALUE = character(1))
+  ) %>%
+  mutate(abbrvs = meta$Study.Abbreviation[match(.$other, meta$TSS.Code)])
+
+example_set <- example_set %>%
+  mutate(abbrvs = ifelse(is.na(abbrvs), tss_meta$BCR[is.na(tss_meta$TSS.Code)], abbrvs))
+
+
+# Get the total number of rows in example_set
+total_rows <- nrow(example_set)
+
+# Calculate 70% of the total rows
+num_rows_to_select <- round(0.25 * total_rows)
+
+# Get a random sample of row indices
+set.seed(99)
+random_indices <- sample(1:total_rows, num_rows_to_select)
+
+combined_table <- example_set %>%
+  select("abbrvs") %>%
+  mutate(group = ifelse(!row_number() %in% random_indices, "Train", "Test")) %>%
+  group_by(group, abbrvs) %>%
+  summarise(Count = n(), .groups = "drop") %>%
+  group_by(group) %>%
+  mutate(Countp = round(Count / sum(Count), 3)) %>%
+  ungroup()
+
+
+plt2 <- ggplot(combined_table, aes(fill = group, x = abbrvs, y = Count)) +
+  geom_bar(stat = "identity")
+ggsave("sample_dist.pdf", width = 50, height = 10)
+
